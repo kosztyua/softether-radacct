37735f0b498b2a16f9b9126bb4ffc08e56461b53
m0zart89 <mozart-freelance@yandex.ru>
2015-12-21 16:12:00 +0200

    Update acctstart.php

--- acctstart.php
+++ acctstart.php
@@ -1,3 +1,4 @@
+<?php
 /*
  * SoftEther RADIUS accounting PHP script
  * Copyright (C) 2015 Andras Kosztyu (kosztyua@vipcomputer.hu)
@@ -17,29 +18,31 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
   
-<?php
 require_once("settings.php");
 require_once("functions.php");
+
 while( $input = readline() ) {
+    
   $pid = pcntl_fork();
   if ($pid === -1) { die(); }
   elseif ($pid === 0) {
-    $re1='.*?((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?![\\d]).*?".*?".*?(".*?")';
+    $re1 = '\d (\S+) \[.*The new session \"(.+)\" has been created.';
     if ($c=preg_match_all ("/".$re1."/is", $input, $matches))
     {
-      $softetherip=$matches[1][0];
-      $sessid=trim($matches[2][0],'"');
+      	$softetherip=$matches[1][0];
+	$sessid=$matches[2][0];
     }
+    
     if (empty($sessid)) { exit; }
     
     $result = getsessiondata($sessid); // get session details from HUB
     $framedip = getdhcpip($sessid); // get DHCP assigned IP from HUB 
-
+    
     if ($framedip === FALSE) { // if user could not get ip with dhcp, disconnect it 
       disconnectsession($sessid);
       exit; 
     }
-
+    
     $db = new SQLite3($database);
     $db->busyTimeout(5000);
     $db->exec('CREATE TABLE IF NOT EXISTS sessions (sessionid varchar(255), username varchar (255), clientip varchar (255), inputoctets varchar (255), ' .
@@ -72,10 +75,12 @@ while( $input = readline() ) {
               "NAS-IP-Address = ".$results['nasip']."\n";
     fwrite($handle, $packet);
     fclose($handle);
+
     exec("radclient ".$radiussrv.":".$radiusport." acct ".$radiuspass." -f ".$tmpfname);
     unlink($tmpfname);
   
     $db->close();
+    exit(0);
   }
 }
 
